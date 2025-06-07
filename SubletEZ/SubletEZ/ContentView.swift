dfd5178dda0e1b7f0ef74d63bb778ec7e914f8de
Soham Kalgaonkar <sohamkal123@gmail.com>
2025-06-07 00:41:52 -0600

    adding the initial code file

---
+++ SubletEZ/SubletEZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubletEZ
+//
+//  Created by Soham Kalgaonkar on 2025-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubletEZ/SubletEZTests/SubletEZTests.swift
@@ -0,0 +1,17 @@
+//
+//  SubletEZTests.swift
+//  SubletEZTests
+//
+//  Created by Soham Kalgaonkar on 2025-06-06.
+//
+
+import Testing
+@testable import SubletEZ
+
+struct SubletEZTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
