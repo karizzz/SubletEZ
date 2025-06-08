4f581c18e26e6ef5d2b56597e92ce38e275b0c75
Soham Kalgaonkar <sohamkal123@gmail.com>
2025-06-08 16:54:31 -0600

    adding firebase files

--- SubletEZ/SubletEZ/SubletEZApp.swift
+++ SubletEZ/SubletEZ/SubletEZApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct SubletEZApp: App {
+    // Register AppDelegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+// Create AppDelegate for Firebase configuration
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
