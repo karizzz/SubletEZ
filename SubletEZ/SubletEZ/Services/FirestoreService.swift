3af1ff8f907a7a89bdb79cf1e6a470ad2caf8029
kartik <kartiksaxena.wps@gmail.com>
2025-06-18 16:14:12 -0600

    db connected

--- SubletEZ/SubletEZ/Services/FirestoreService.swift
+++ SubletEZ/SubletEZ/Services/FirestoreService.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+
+
+class FirestoreService {
+    static let shared = FirestoreService()
+    private init() {}
+    
+    private let db =  Firestore.firestore()
+    
+    func addListing(data : [ String : Any], completion: @escaping (Result<Void, Error>)-> Void){
+        db.collection("sublets").addDocument(data: data) {error in
+            if let error = error {
+                completion(.failure(error))
+                
+            }else{
+                completion(.success(()))
+            }
+        }
+        
+    }
+}
